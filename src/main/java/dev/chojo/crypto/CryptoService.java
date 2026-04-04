4f53b968ff6e641162f4d8165b6b79f2bb05b7f6
Nora <46890129+RainbowDashLabs@users.noreply.github.com>
2026-04-04 17:50:40 +0200

    Allow serialization and deserialization of keys to and from string

--- src/main/java/dev/chojo/crypto/CryptoService.java
+++ src/main/java/dev/chojo/crypto/CryptoService.java
@@ -9,31 +9,42 @@ import com.google.inject.Inject;
 import dev.chojo.configuration.Configuration;
 import dev.chojo.crypto.exceptions.CryptoException;
 
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 
 public class CryptoService {
     private final Configuration configuration;
     private final KeyPairGenerator generator;
+    private final KeyFactory kf;
 
     @Inject
     public CryptoService(Configuration configuration) throws NoSuchAlgorithmException {
         this.configuration = configuration;
         generator = KeyPairGenerator.getInstance(key());
         generator.initialize(keySize(), new SecureRandom());
+        kf = KeyFactory.getInstance(key());
+
     }
 
     public KeyPair generateKeyPair() {
@@ -62,7 +73,7 @@ public class CryptoService {
 
     private byte[] process(byte[] data, int opMode, Key key)
             throws NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException,
-                    InvalidKeyException {
+            InvalidKeyException {
         Cipher cipher = Cipher.getInstance(cipher());
         cipher.init(opMode, key);
         cipher.update(data);
@@ -71,6 +82,28 @@ public class CryptoService {
 
     public String decryptString(byte[] data, PrivateKey key) {
         return new String(decrypt(data, key), StandardCharsets.UTF_8);
+    }
+
+    public String serializeKey(Key key) {
+        String encoded = Base64.getEncoder().encodeToString(key.getEncoded());
+        if (key instanceof PublicKey) {
+            encoded = "-----BEGIN PUBLIC KEY-----\n" + encoded + "\n-----END PUBLIC KEY-----";
+        } else if (key instanceof PrivateKey) {
+            encoded = "-----BEGIN PRIVATE KEY-----\n" + encoded + "\n-----END PRIVATE KEY-----";
+        }
+        return encoded;
+    }
+
+    public PrivateKey deserializePrivateKey(String key) throws InvalidKeySpecException {
+        key = key.replace("\n", "").replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "").trim();
+        byte[] decoded = Base64.getDecoder().decode(key);
+        return kf.generatePrivate(new PKCS8EncodedKeySpec(decoded));
+    }
+
+    public PublicKey deserializePublicKey(String key) throws InvalidKeySpecException {
+        key = key.replace("\n", "").replace("-----BEGIN PUBLIC KEY-----", "").replace("-----END PUBLIC KEY-----", "").trim();
+        byte[] decoded = Base64.getDecoder().decode(key);
+        return kf.generatePublic(new X509EncodedKeySpec(decoded));
     }
 
     private String cipher() {
--- src/test/java/dev/chojo/crypto/CryptoServiceTest.java
+++ src/test/java/dev/chojo/crypto/CryptoServiceTest.java
@@ -10,8 +10,12 @@ import dev.chojo.configuration.elements.Root;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.security.Key;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -46,5 +50,41 @@ class CryptoServiceTest {
         byte[] bytes = cryptoService.encrypt("Test String", keyPair.getPublic());
         String decrypted = cryptoService.decryptString(bytes, keyPair.getPrivate());
         assertEquals("Test String", decrypted);
+    }
+
+    @Test
+    void serializePublicKey() {
+        Key key = cryptoService.generateKeyPair().getPublic();
+        String serializedKey = cryptoService.serializeKey(key);
+        System.out.println(serializedKey);
+        assertNotNull(serializedKey);
+    }
+
+    @Test
+    void serializePrivateKey() {
+        Key key = cryptoService.generateKeyPair().getPublic();
+        String serializedKey = cryptoService.serializeKey(key);
+        System.out.println(serializedKey);
+        assertNotNull(serializedKey);
+    }
+
+    @Test
+    void deserializePublicKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        Key original = cryptoService.generateKeyPair().getPublic();
+        String serializedKey = cryptoService.serializeKey(original);
+        PublicKey key = cryptoService.deserializePublicKey(serializedKey);
+        assertEquals(original.getAlgorithm(), key.getAlgorithm());
+        assertEquals(original.getFormat(), key.getFormat());
+        assertArrayEquals(original.getEncoded(), key.getEncoded());
+    }
+    @Test
+    void deserializePrivateKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+        Key original = cryptoService.generateKeyPair().getPrivate();
+        String serializedKey = cryptoService.serializeKey(original);
+        PrivateKey key = cryptoService.deserializePrivateKey(serializedKey);
+        assertEquals(original.getAlgorithm(), key.getAlgorithm());
+        assertEquals(original.getFormat(), key.getFormat());
+        assertArrayEquals(original.getEncoded(), key.getEncoded());
+
     }
 }
